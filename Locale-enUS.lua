dec33df78c127e5198f0cc29631b2159dc6f4168
Nyctef <markcjordan@gmail.com>
2009-08-07 14:00:13 +0100

    first commit

---
+++ Locale-enUS.lua
@@ -0,0 +1,16 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("AutoBuffsDruid", "enUS", true)
+
+-- Player states
+L["buffed"] = "Buffed"
+L["fakedc"] = "Offline"
+L["deadjim"] = "Dead"
+L["farfaraway"] = "Out of Range"
+L["wantsbuffing"] = "Need Buffing"
+L["nomming"] = "Eating"
+L["quaffing"] = "Drinking"
+L["oooooooh"] = "Ghost"
+L["taxiiing"] = "On a Taxi"
+L["riding"] = "Mounted"
+L["sleeping"] = "Rested"
+L["sneaky"] = "Stealthed"
+
